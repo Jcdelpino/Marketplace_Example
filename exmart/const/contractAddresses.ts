0a8c725c48ee606efbe51bbbb8fbf7a734866b54
Jcdelpino <jcdelpino@hotmail.com>
2023-06-07 16:47:59 -0400

    Changes to be committed: 	new file:   exmart/.eslintrc.json 	new file:   exmart/.gitignore 	new file:   exmart/.prettierignore 	new file:   exmart/LICENSE.md 	new file:   exmart/README.md 	new file:   exmart/components/Container/Container.module.css 	new file:   exmart/components/Container/Container.tsx 	new file:   exmart/components/ListingWrapper/ListingWrapper.tsx 	new file:   exmart/components/NFT/NFT.module.css 	new file:   exmart/components/NFT/NFT.tsx 	new file:   exmart/components/NFT/NFTGrid.tsx 	new file:   exmart/components/Navbar/Navbar.module.css 	new file:   exmart/components/Navbar/Navbar.tsx 	new file:   exmart/components/SaleInfo/Sale.module.css 	new file:   exmart/components/SaleInfo/SaleInfo.tsx 	new file:   exmart/components/Skeleton/Skeleton.module.css 	new file:   exmart/components/Skeleton/Skeleton.tsx 	new file:   exmart/const/contractAddresses.ts 	new file:   exmart/next-env.d.ts 	new file:   exmart/next.config.js 	new file:   exmart/package.json 	new file:   exmart/pages/_app.tsx 	new file:   exmart/pages/buy.tsx 	new file:   exmart/pages/index.tsx 	new file:   exmart/pages/profile/[address].tsx 	new file:   exmart/pages/sell.tsx 	new file:   exmart/pages/token/[contractAddress]/[tokenId].tsx 	new file:   exmart/public/favicon.ico 	new file:   exmart/public/hero-asset.png 	new file:   exmart/public/hero-gradient.png 	new file:   exmart/public/logo.png 	new file:   exmart/public/thirdweb.svg 	new file:   exmart/public/user-icon.png 	new file:   exmart/styles/Buy.module.css 	new file:   exmart/styles/Home.module.css 	new file:   exmart/styles/Profile.module.css 	new file:   exmart/styles/Sale.module.css 	new file:   exmart/styles/Token.module.css 	new file:   exmart/styles/globals.css 	new file:   exmart/tsconfig.json 	new file:   exmart/util/formatTimeRemaining.ts 	new file:   exmart/util/randomColor.ts 	new file:   exmart/util/toastConfig.ts 	new file:   exmart/yarn.lock

---
+++ exmart/const/contractAddresses.ts
@@ -0,0 +1,18 @@
+/** Replace the values below with the addresses of your smart contracts. */
+
+// 1. Set up the network your smart contracts are deployed to.
+// First, import the chain from the package, then set the NETWORK variable to the chain.
+import { Mumbai, Polygon } from "@thirdweb-dev/chains";
+export const NETWORK = Polygon;
+
+// 2. The address of the marketplace V3 smart contract.
+// Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
+export const MARKETPLACE_ADDRESS = "0x646df0eA6e54EDEebD53f1C757006379f172DA0F";
+
+// 3. The address of your NFT collection smart contract.
+export const NFT_COLLECTION_ADDRESS =
+  "0xDC6989841E62bAd4536570b7f0fb75Eb60467a7e";
+
+// (Optional) Set up the URL of where users can view transactions on
+// For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
+export const ETHERSCAN_URL = "https://polygonscan.com";
